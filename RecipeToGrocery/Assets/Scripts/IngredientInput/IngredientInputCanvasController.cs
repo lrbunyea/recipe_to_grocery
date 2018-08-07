cf3e27f1457a0b445c4126a805582c4b6ceda095
Leo Bunyea <lrbunyea@wpi.edu>
2018-08-07 16:44:36 -0400

    Ingredient input screen now resets after saving a recipe.

--- RecipeToGrocery/Assets/Scripts/IngredientInput/IngredientInputCanvasController.cs
+++ RecipeToGrocery/Assets/Scripts/IngredientInput/IngredientInputCanvasController.cs
@@ -12,13 +12,21 @@ public class IngredientInputCanvasController : MonoBehaviour {
 
     //OBJECTS TO LINK IN EDITOR
     [SerializeField] InputField ingredientInputField;
+    [SerializeField] InputField recipeNameInputField;
     [SerializeField] Button addIngredientButton;
 
     //PRIVATE
     private RectTransform buttonRectTransform;
-    private float xPos;
-    private float yPos;
+    Vector2 newPosition;
+    Vector2 originalPosition;
     private int currentNumInputFields = 0;
+    #endregion
+
+    #region Unity API Functions
+    void Awake()
+    {
+        GetInitalButtonInformation();
+    }
     #endregion
 
     #region Button Press Functions
@@ -28,13 +36,13 @@ public class IngredientInputCanvasController : MonoBehaviour {
     /// </summary>
     public void InputAnotherIngredient()
     {
-        GetButtonInformation();
+        UpdateButtonInfo();
         //Instantiate input field at the location of the of button
         InputField field;
         field = Instantiate(ingredientInputField, buttonRectTransform.anchoredPosition, Quaternion.identity);
         field.transform.SetParent(transform, false);
         //Shift button down
-        buttonRectTransform.anchoredPosition = new Vector2(xPos, yPos);
+        buttonRectTransform.anchoredPosition = newPosition;
         currentNumInputFields++;
 
         //Check after index is increased
@@ -44,37 +52,68 @@ public class IngredientInputCanvasController : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Fetches the recipe name and ingredients from the input fields then writes them to the file.
+    /// After this, it refreshes the ingredient input screen.
+    /// </summary>
     public void SaveIngredients()
     {
-        //variables to be passed to FileIOManager
-        string recipeName;
-        recipeName = FindObjectOfType<RecipeNameInputFieldController>().FetchRecipeName();
-        Debug.Log(recipeName);
-
-        //Rip ingredients from all input fields
-        IngredientInputFieldController[] data = FindObjectsOfType<IngredientInputFieldController>();
-        string[] ingredientData = new string[data.Length];
-        for (int i = 0; i < data.Length; i++)
-        {
-            ingredientData[i] = data[i].FetchIngredient();
-            Debug.Log(ingredientData[i]);
-        }
-
-        FileIOManager.Instance.SaveRecipe(recipeName, ingredientData);
+        string recipe = recipeNameInputField.text;
+        FileIOManager.Instance.SaveRecipe(recipe, FetchIngredientInput());
+        ResetInputScreen();
     }
     #endregion
 
     #region Helper Functions
     /// <summary>
-    /// Gets the rect transform of the "add ingredient" button and then calculates the x and y values to move the button to.
+    /// Gets the rect transform of the "add ingredient" button then stores its original anchored position.
     /// </summary>
-    private void GetButtonInformation()
+    private void GetInitalButtonInformation()
     {
-        //To use in instantiating input fields
         buttonRectTransform = addIngredientButton.GetComponent<RectTransform>();
-        //New x and y values to shift the button to
-        xPos = buttonRectTransform.anchoredPosition.x;
-        yPos = buttonRectTransform.anchoredPosition.y - INPUT_FIELD_SPACING;
+        originalPosition = buttonRectTransform.anchoredPosition;
+    }
+
+    /// <summary>
+    /// Calculates, based on the ingredients current position, the new position to shift the button to.
+    /// </summary>
+    private void UpdateButtonInfo()
+    {
+        float newXPos = buttonRectTransform.anchoredPosition.x;
+        float newYPos = buttonRectTransform.anchoredPosition.y - INPUT_FIELD_SPACING;
+        newPosition = new Vector2(newXPos, newYPos);
+    }
+
+    /// <summary>
+    /// Resets the field number counter, resets the add ingredient back to its original position and activates it.
+    /// Also resets the recipe name input field.
+    /// </summary>
+    private void ResetInputScreen()
+    {
+        currentNumInputFields = 0;
+        buttonRectTransform.anchoredPosition = originalPosition;
+        addIngredientButton.gameObject.SetActive(true);
+        recipeNameInputField.text = "";
+    }
+
+    /// <summary>
+    /// Finds all of the input fields on the ingredient input screen.
+    /// Deletes the actual gameobjects as it saves their data to an array, then DESTROYS the associated input field.
+    /// </summary>
+    /// <returns>The ingredients as an array of strings.</returns>
+    private string[] FetchIngredientInput()
+    {
+        //Fetch all input fields
+        IngredientInputFieldController[] data = FindObjectsOfType<IngredientInputFieldController>();
+
+        //Rip ingredients from all input fields, then delete the input fields themselves
+        string[] ingredientData = new string[data.Length];
+        for (int i = 0; i < data.Length; i++)
+        {
+            ingredientData[i] = data[i].FetchIngredient();
+            Destroy(data[i].gameObject);
+        }
+        return ingredientData;
     }
     #endregion
 }
